d145ce2403e0d94d4926eb8510bc4966fbf1bfca
Marino Faggiana <marino@marinofaggiana.com>
2023-01-14 11:52:54 +0100

    coding
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- Sources/Mantis/CropViewController/CropViewController.swift
+++ Sources/Mantis/CropViewController/CropViewController.swift
@@ -457,10 +457,10 @@ extension CropViewController {
         cropToolbar.translatesAutoresizingMaskIntoConstraints = false
         cropView.translatesAutoresizingMaskIntoConstraints = false
         
-        stackView?.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        stackView?.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
-        stackView?.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor).isActive = true
-        stackView?.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor).isActive = true
+        stackView?.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        stackView?.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        stackView?.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        stackView?.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
     }
     
     fileprivate func setStackViewAxis() {
