5877c382bbeca160b51b5707e9289dbd204b6e56
Marino Faggiana <marino@marinofaggiana.com>
2023-01-14 11:26:17 +0100

    coding
    Signed-off-by: Marino Faggiana <marino@marinofaggiana.com>

--- Sources/Mantis/CropView/CropOverlayView.swift
+++ Sources/Mantis/CropView/CropOverlayView.swift
@@ -9,8 +9,8 @@
 import UIKit
 
 class CropOverlayView: UIView {
-    private var boarderNormalColor = UIColor.gray
-    private var boarderHintColor = UIColor.gray
+    private var boarderNormalColor = UIColor.white
+    private var boarderHintColor = UIColor.white
     private var hintLine = UIView()
     private var tappedEdge: CropViewOverlayEdge = .none
     
